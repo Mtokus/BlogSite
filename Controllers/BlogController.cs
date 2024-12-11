f04943cc2717d51ba0fe9d1311cab0853385bfea
mtoku <mtoku@DESKTOP-PT0KF0R>
2024-12-11 10:41:33 +0300

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager bm= new BlogManager(new EfBlogRepository());
+
+        public IActionResult Index()
+        {
+            var values =bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
